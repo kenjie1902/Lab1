f8a5f83f4e2dcf2df7ca97d9f51b413bf403e1f6
kenjie1902 <145912922+kenjie1902@users.noreply.github.com>
2023-10-18 10:38:06 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,66 @@
+//REST API demo in Node.js
+var express = require('express'); // requre the express framework
+var app = express();
+var fs = require('fs'); //require file system object
+
+// Endpoint to Get a list of users
+app.get('/getUsers', function(req, res){
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data){
+        console.log(data);
+        res.end(data); // you can also use res.send()
+    });
+})
+
+// Create a server to listen at port 8080
+var server = app.listen(8080, function(){
+    var host = server.address().address
+    var port = server.address().port
+    console.log("REST API demo app listening at http://%s:%s", host, port)
+})
+
+//Step 1: Create a new user variable
+var user = {
+    "user6": {
+        "id":6,
+        "company name:":"Microsoft",
+        "position":"Assistant Vice President",
+        "location":"Toril",
+        "employee":"Jake Coronado"
+      },
+} 
+
+//The addUser endpoint
+app.post('/addUser', function(req, res){
+    //Step 2: read existing users
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data){
+        data = JSON.parse(data);
+        //Step 3: append user variable to list
+        data["user6"] = user["user6"];
+        console.log(data);
+        res.end(JSON.stringify(data));
+    });
+})
+
+//Endpoint to get a single user by id
+app.get('/:id', function (req, res) {
+    // First retrieve existing user list
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       var users = JSON.parse( data );
+       var user = users["user" + req.params.id] 
+       console.log( user );
+       res.end( JSON.stringify(user));
+    });
+ })
+
+  //Code to delete a user by id
+  var id = 5;
+  app.delete('/:id', function (req, res) {
+     // First retrieve existing users
+     fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+        data = JSON.parse( data );
+        delete data["user" + 5];
+         
+        console.log( data );
+        res.end( JSON.stringify(data));
+     });
+  })
